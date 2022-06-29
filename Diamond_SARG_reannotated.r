767faf657943c3244c06071bbccb79decc38de2a
ianyen77 <96662135+ianyen77@users.noreply.github.com>
2022-06-29 21:15:12 +0800

    Create Diamond_SARG_reannotated.r

---
+++ Diamond_SARG_reannotated.r
@@ -0,0 +1,17 @@
+library("openxlsx")
+library("tidyverse")
+x1<-read.xlsx(xlsxFile = "C://Users/USER/Desktop/Practicedate_Blastx_SARG2.2_output.xlsx",sheet=2,colNames = T,sep="_")
+x1$Corresponding_ids<-gsub(pattern='\\[',replacement="",x1$Corresponding_ids)
+x1$Corresponding_ids<-gsub(pattern='\\]',replacement="",x1$Corresponding_ids)
+x1$Corresponding_ids<-gsub(pattern="\\'",replacement="",x1$Corresponding_ids)
+x1$Corresponding_ids<-gsub(pattern=" ",replacement="",x1$Corresponding_ids)
+c<-separate(x1,Corresponding_ids,into = paste0("COL1_", 1:1000),sep=",")
+help(gather)
+cc1<-c%>%
+  gather(paste0("COL1_", 1:1000),key="fakename",value="gene",na.rm=T)
+cc1$fakename<-NULL
+write.xlsx(cc1,"c:/Users/USER/Desktop/SARGdatabase.xlsx",colnames=T)
+Diamond_SARG_hit<-read.xlsx(xlsxFile = "C://Users/USER/Desktop/Practicedate_Blastx_SARG2.2_output.xlsx",sheet=1,colNames = F,sep="_")
+colnames(Diamond_SARG_hit)<-c("qseqid", "gene", "pident", "length", "mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore")
+Diamond_SARG_hit_annoate<-merge(Diamond_SARG_hit,cc1,all.x = T)
+write.xlsx(Diamond_SARG_hit_annoate,"c:/Users/USER/Desktop/Diamond_SARG_annotate.xlsx",colnames=T)
